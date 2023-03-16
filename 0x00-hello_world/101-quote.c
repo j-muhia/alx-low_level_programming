2e5d9d06dc1cf619557fc0f4818d300aa5cc96ca
j-muhia <jesse.muhia@gmail.com>
2023-03-16 14:26:50 +0000

    C - Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
   *main - Prints and that piece of art is useful" - Dora Korpar, 2015-10-19
   *Return: value is 1
   */
-  int main(void)
-	  {
-		            write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-			    return (1);
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (1);
+}
