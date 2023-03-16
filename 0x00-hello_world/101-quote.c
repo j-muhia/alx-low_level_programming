3817824712bb815d2cfd0ba5f7fc04c6c9fcf0c0
j-muhia <jesse.muhia@gmail.com>
2023-03-16 13:04:45 +0000

    C - Hello World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+  *main - Prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+  *Return: value is 1
+  */
+  int main(void)
+	  {
+		            write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+			    return (1);
