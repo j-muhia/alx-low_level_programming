2892cff6ae1b70b33fc752b2e133c9d3384b0b5b
j-muhia <jesse.muhia@gmail.com>
2023-03-27 18:29:40 +0000

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line
+ *@str: The string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse
+ * @s: The string to print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	for (a -= 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n')
+}
